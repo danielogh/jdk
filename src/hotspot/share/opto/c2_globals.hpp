25af3826ff691aad91de048426a877cfd8c12a2d
Daniel Skantz <daniel.skantz@oracle.com>
2026-08-20 10:05:33 +0000

    bracket

--- src/hotspot/share/opto/c2_globals.hpp
+++ src/hotspot/share/opto/c2_globals.hpp
@@ -484,7 +484,7 @@
                                                                             \
   develop(bool, StressRecompilation, false,                                 \
           "Recompile each compiled method while successively disabling "    \
-          "selected optimizations.                                          \
+          "selected optimizations.")                                        \
                                                                             \
   develop(intx, ImplicitNullCheckThreshold, 3,                              \
           "Don't do implicit null checks if NPE's in a method exceeds "     \
